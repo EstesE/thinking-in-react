41889475f61022444ee590812fd3f5e8ae44f7b4
Mithrilhall <eric_estes@aspensquare.com>
2023-03-16 00:33:37 -0400

    Initial commit

---
+++ components/filterableProductTable.js
@@ -0,0 +1,13 @@
+import SearchBar from "./searchBar";
+import ProductTable from "./productTable";
+
+const FilterableProductTable = ({ products }) => {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+};
+
+export default FilterableProductTable;
